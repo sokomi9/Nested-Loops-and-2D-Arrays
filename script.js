8ebbc5689bbbd24e7d069aa6b8ae4d4a54920dd3
Abiud Sokomi <abiudsokomi@gmail.com>
2023-11-22 06:28:12 +0300

    my commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var numberGrid = [
+    [1,2,3],
+    [4,5,6],
+    [7,7,7],
+    [0]
+];
+for (let i = 0; i < numberGrid.length; i++) {
+    for (let j = 0; j < numberGrid[i].length; j++) {
+        document.write(numberGrid[i][j]);
+    }
+    document.write("<br/>");
+    
+}
